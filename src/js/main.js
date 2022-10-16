09de16255e181da45305c050da81bfab0fe700b1
a-klimovich <kom9toshka@gmail.com>
2022-10-16 20:54:38 +0200

    dellet:  usles file

--- src/js/main.js
+++ src/js/main.js
@@ -27,71 +27,40 @@
     // Header menu controllers
     $burger.on("click", () => {
       $burger.toggleClass("burger__active");
-      $searchInput.toggleClass('active');
-      $logoImage.toggleClass('is-hidden');
+      $searchInput.toggleClass("active");
+      $logoImage.toggleClass("is-hidden");
       $headerNav.toggleClass(cssClassOpen);
     });
 
-    const $searchInput = $('.form__label');
-    const $logoImage = $('.logo a');
+    const $searchInput = $(".form__label");
+    const $logoImage = $(".logo a");
 
     $submenuHeaderBtn.on("click", () => {
       if ($burger.hasClass("burger__active")) {
         $burger.toggleClass("burger__active");
         $headerNav.toggleClass(cssClassOpen);
       }
-      $searchInput.toggleClass('active');
-      $logoImage.toggleClass('is-hidden');
+      $searchInput.toggleClass("active");
+      $logoImage.toggleClass("is-hidden");
     });
-
-    // card open
-    const questionsCard = $(".questions-card")
-    
-    questionsCard.each((i, elem) => {
-      const btnOpenAnswer = $(".btnOpenAnswer", elem);
-      const contentAnswer = $(".content__answer", elem);
-      
-      btnOpenAnswer.on("click", () => {
-        btnOpenAnswer.toggleClass(cssClassActive);
-        contentAnswer.toggleClass(cssClassIsHidden);
-      });
-    })
-
-    // PRODUCT CARD LIST ITEM COUNTER
-    const $productListCard = $('.production-list-card');
-
-    $productListCard.each((i, elem)  => {
-      const listLength = $('.list-menu li', elem).length;
-      const $counterNode = $('.counter', elem);
-      
-      
-      $(elem).on('click', () => {
-        $(elem).toggleClass(cssClassActive)
-      })
-
-      if (listLength === 0) {
-        $('.list-menu', elem).remove()
-        $('.list-item-counter', elem).remove()
-      }
-
-      $counterNode[0].innerHTML = `${listLength}`;
-    })
 
     // SLIDERS
     $("#mainSlider").slick({
       slidesToShow: 1,
-      arrows: false,
+      // arrows: false,
+      prevArrow: '<span class="arrow prev"><img src="../../assets/arrow.svg" alt=""></span>',
+      nextArrow: '<span class="arrow next"><img src="../../assets/arrow.svg" alt=""></span>',
       dots: true,
       responsive: [
         {
           breakpoint: 1200,
           settings: {
             dots: false,
-            arrows: false,
-            slidesToShow: 1
-          }
+            // arrows: false,
+            slidesToShow: 1,
+          },
         },
-      ]
+      ],
     });
 
     $("#logoSlider").slick({
@@ -106,7 +75,7 @@
             arrows: false,
             slidesToShow: 5,
             slidesToScroll: 5,
-          }
+          },
         },
         {
           breakpoint: 996,
@@ -115,14 +84,14 @@
             arrows: false,
             slidesToShow: 3,
             slidesToScroll: 3,
-          }
+          },
         },
         {
           breakpoint: 685,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
-          }
+          },
         },
         {
           breakpoint: 520,
@@ -130,9 +99,9 @@
             dots: true,
             slidesToShow: 1,
             slidesToScroll: 1,
-          }
+          },
         },
-      ]
+      ],
     });
 
     $("#sliderProduct").slick({
@@ -146,47 +115,24 @@
           settings: {
             slidesToScroll: 1,
             slidesToShow: 1,
-          }
+          },
         },
-      ]
-    });
-    
-    $("#managmentStuff").slick({
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      dots: true,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToScroll: 2,
-            slidesToShow: 2,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToScroll: 1,
-            slidesToShow: 1,
-          }
-        },
-      ]
+      ],
     });
 
     // PRODUCT ITEM SLIDERs
-    $('#productBigImage').slick({
+    $("#productBigImage").slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       fade: true,
-      asNavFor: '#productSmallImage',
+      asNavFor: "#productSmallImage",
       arrows: true,
     });
 
-    $('#productSmallImage').slick({
+    $("#productSmallImage").slick({
       slidesToShow: 4,
       slidesToScroll: 1,
-      asNavFor: '#productBigImage',
+      asNavFor: "#productBigImage",
       dots: false,
       focusOnSelect: true,
       arrows: false,
