24f76461ec624f5f39de69aac8c0de4862dff93e
a-klimovich <kom9toshka@gmail.com>
2023-02-12 22:20:29 +0100

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,134 @@
 (function () {
+  // Base
+  const cssClassActive = "active";
+  const cssClassOpen = "open";
+  const cssClassIsHidden = "is-hidden";
+
+  // Node's
+  const $body = $("body");
+  const $headerNav = $("#headerNav");
+  const $burger = $(".burger");
+  const $submenuHeaderBtn = $(".submenu-header");
+
+  const mainSliederSettings = {
+    slidesToShow: 1,
+    prevArrow: '<span class="arrow prev"><img src="./assets/arrow.svg" alt=""></span>',
+    nextArrow: '<span class="arrow next"><img src="./assets/arrow.svg" alt=""></span>',
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+  const logoSliderSettings = {
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        },
+      },
+      {
+        breakpoint: 996,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 685,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const sliderProductSettings = {
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToScroll: 2,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 555,
+        settings: {
+          slidesToScroll: 2,
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
+  const achivSliderSettings = {
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    centerPadding: "60px",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToScroll: 2,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   $(document).ready(function () {
-    window.onscroll = () => scrollFunction();
-    // Base
-    const cssClassActive = "active";
-    const cssClassOpen = "open";
-    const cssClassIsHidden = "is-hidden";
-
-    // Node's
-    const $body = $("body")
-    const $headerNav = $("#headerNav");
-    const $burger = $(".burger");
-    const $submenuHeaderBtn = $(".submenu-header");
-
-    // Header on scroll
-    function scrollFunction() {
-      const scrollTop = $(document.body).scrollTop();
-      const scrollTopDocumentElement = $(document.documentElement).scrollTop();
-      const $header = $("header");
-
-      if (scrollTop > 72 || scrollTopDocumentElement > 72) {
-        $header.addClass("scroll");
+    window.onscroll = () => {
+      const scrollTop = $("body").scrollTop() || $("html").scrollTop();
+      if (scrollTop > 72) {
+        $("header").addClass("scroll");
         $body.addClass("scroll");
       } else {
-        $header.removeClass("scroll");
+        $("header").removeClass("scroll");
         $body.removeClass("scroll");
       }
-    }
+    };
 
     // Header menu controllers
     $burger.on("click", () => {
       $burger.toggleClass("burger__active");
-      $searchInput.toggleClass("active");
-      $logoImage.toggleClass("is-hidden");
+      $searchInput.toggleClass(cssClassActive);
+      $logoImage.toggleClass(cssClassIsHidden);
       $headerNav.toggleClass(cssClassOpen);
     });
 
@@ -44,129 +141,24 @@
         $headerNav.toggleClass(cssClassOpen);
       }
 
-      $('.mobile-c-l').toggleClass('active');
+      $(".mobile-c-l").toggleClass(cssClassActive);
     });
 
     // SLIDERS
-    $("#mainSlider").slick({
-      slidesToShow: 1,
-      // arrows: false,
-      prevArrow: '<span class="arrow prev"><img src="./assets/arrow.svg" alt=""></span>',
-      nextArrow: '<span class="arrow next"><img src="./assets/arrow.svg" alt=""></span>',
-      dots: true,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            dots: false,
-            // arrows: false,
-            slidesToShow: 1,
-          },
-        },
-      ],
-    });
-
-    $("#logoSlider").slick({
-      slidesToShow: 5,
-      slidesToScroll: 5,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            dots: true,
-            arrows: false,
-            slidesToShow: 5,
-            slidesToScroll: 5,
-          },
-        },
-        {
-          breakpoint: 996,
-          settings: {
-            dots: true,
-            arrows: false,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          },
-        },
-        {
-          breakpoint: 685,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-          },
-        },
-        {
-          breakpoint: 520,
-          settings: {
-            dots: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
-    });
-
-    $("#sliderProduct").slick({
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      dots: true,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 980,
-          settings: {
-            slidesToScroll: 2,
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 555,
-          settings: {
-            slidesToScroll: 2,
-            slidesToShow: 2,
-          },
-        },
-      ],
-    });
-
-    $("#achivSlider").slick({
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      dots: false,
-      arrows: false,
-      centerMode: true,
-      centerPadding: '60px',
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToScroll: 2,
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToScroll: 1,
-            slidesToShow: 1,
-          },
-        },
-      ],
-    });
-
-    
-    
+    $("#mainSlider").slick(mainSliederSettings);
+    $("#logoSlider").slick(logoSliderSettings);
+    $("#sliderProduct").slick(sliderProductSettings);
+    $("#achivSlider").slick(achivSliderSettings);
   });
 
-  $(".pagination .pagination__number.active").click((event) => event.preventDefault());
+  $(".pagination .pagination__number.active")?.click((event) => event.preventDefault());
 })();
 
 // PRODUCT ITEM SLIDERs
-window.addEventListener('load', function() {
-  const productBigImageSlider = $('productBigImage');
-  const productSmallImageSlider = $('productSmallImage');
-  const bugetImageContainer = $('.show-in-modal');
+window.addEventListener("load", function () {
+  const productBigImageSlider = $("productBigImage");
+  const productSmallImageSlider = $("productSmallImage");
+  const bugetImageContainer = $(".show-in-modal");
 
   if (productBigImageSlider && productSmallImageSlider && bugetImageContainer) {
     $("#productBigImage").slick({
@@ -184,7 +176,7 @@ window.addEventListener('load', function() {
       focusOnSelect: true,
       arrows: false,
     });
-  
-    baguetteBox.run('.show-in-modal');
+
+    baguetteBox.run(".show-in-modal");
   }
 });
